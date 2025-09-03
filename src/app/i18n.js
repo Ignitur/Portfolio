8c000127fc66b8c27df84ffe5559ff0d58a68519
Maurszky Mihály <maurszkymihaly@gmail.com>
2025-09-03 06:09:52 +0200

    init multilingual i18n

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -1,18 +1,25 @@
+import en from './locales/en/common.json';
+import ru from './locales/ru/common.json';
+import hu from './locales/hu/common.json';
+
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
-
+	
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: true,
+		debug: true,
     fallbackLng: "en",
     interpolation: {
-      escapeValue: false,
+			escapeValue: false,
     },
+		resources: {
+			en: { common: en },
+			ru: { common: ru },
+			hu: { common: hu },
+		},
   });
 
 export default i18n;
